2eec6319795f5ee3a512e0f141e2099065c5206d
KacemiHezo <147346368+KacemiHezo@users.noreply.github.com>
2024-06-24 03:31:48 +0200

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,25 @@
+public class Map
+{
+    public Point Origin { get; set;}
+    public Point Size { get; }
+
+    private int[][] mapData;
+    private Dictionary<CellType, char> cellVisuals = new Dictionary<CellType, char>{
+        { CellType.Empty, ' '},
+        { CellType.WallHorizontal, '-'},
+        { CellType.WallVertical, '|'},
+        { CellType.WallCorner, '+'},
+        { CellType.Teleport, '♨'},
+        { CellType.Floor, '.'},
+        { CellType.Water, '⭕'},
+        { CellType.Shop, '💰'},
+
+    }
+    private CellType[] walkableCellTypes = new CellType[] { 
+        CellType.Floor, 
+        CellType.Grass,
+        CellType.Teleport,
+        CellType.Shop,
+    };
+    
+    };
