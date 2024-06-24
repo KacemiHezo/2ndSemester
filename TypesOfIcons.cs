14e18837adfad3be4505d895dd50e14848436c0d
KacemiHezo <147346368+KacemiHezo@users.noreply.github.com>
2024-06-24 03:32:15 +0200

    Adding Icons

---
+++ TypesOfIcons.cs
@@ -0,0 +1,12 @@
+public enum CellType
+{
+    Empty = 0,
+    WallHorizontal = 1,
+    WallVertical = 2,
+    WallCorner = 3,
+    Teleport = 4,
+    Floor = 5,
+    Water = 6,
+    Player = 7,
+    Shop = 8,
+}
