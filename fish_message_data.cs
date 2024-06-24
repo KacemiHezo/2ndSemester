bb545a2a980af97ed13de6c1a3f7b634b92b943a
KacemiHezo <147346368+KacemiHezo@users.noreply.github.com>
2024-06-24 22:10:27 +0200

    Fishing Data from Max

---
+++ fish_message_data.cs
@@ -0,0 +1,53 @@
+﻿//the fish date base, there are 6 fish in total, 3 common ones and 3 rare one
+//the way it's displayed is like this
+//the fish number, used for 2 things, 1 showing what rod can get what fish and 2 makin the code easire to make [instead of makin new ints u just change the fish number] 
+//the fish name, the name of the fish
+//the fish rare, the raity of the fish this depands on the cost of the fish when selling it
+//the fish txt, the txt that will be shown when the fish is caught
+public numberwater
+if numberwater
+//fish 1
+if (fishnumber = 1)
+{
+string fish_name = ("araneacis");
+int fish_rare = common;
+string fish_txt = ("a fish with 8 fins and 2 stag beetles like pincers, they made good pets");
+}
+
+//fish 2
+if (fishnumber = 2)
+{
+string fish_name = ("sicmn");
+int fish_rare = common;
+string fish_txt = ("a black and white fish, looks skinny, almost like a skeleton, it's patterns are used for shoes");
+}
+//fish 3
+if (fishnumber = 3)
+{
+string fish_name = ("pislique");
+int fish_rare = common;
+string fish_txt = ("a fish whose scales look like they are melting, usually used a spice, it has a weird sweet and spicy taste");
+}
+
+//fish 4
+if (fishnumber = 4)
+{
+string fish_name = ("chocolate shark");
+int fish_rare = rare;
+string fish_txt = ("a shark that lives in swamps, its name comes from the pattern on its skin, it's toxic to eat");
+}
+//fish 5
+if (fishnumber = 5)
+{
+string fish_name = ("wuopod");
+int fish_rare = rare;
+string fish_txt = ("a octopus like animal that has two main tenticals and 6 smaller ones. They like to make human faces out of kelp");
+}
+//fish 6
+if (fishnumber = 6)
+{
+string fish_name = ("crocodish");
+int fish_rare = rare;
+string fish_txt = ("a crocodile who's spikes look like a salad, a lot of people often eat them alive");
+}
+
